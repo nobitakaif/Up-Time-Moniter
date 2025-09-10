c8131279942ded08b12ea99dfa5565a06f83f147
mohd kaif <mk2818356@gmail.com>
2025-09-10 23:50:35 +0530

    fixed middleware syntax

--- apps/http/middleware.ts
+++ apps/http/middleware.ts
@@ -2,20 +2,22 @@ import type { Request, Response, NextFunction } from "express"
 import jwt from "jsonwebtoken"
 import { JWT_PUBLIC_KEY } from "./config"
 
-export function authMiddleware(req:Request,res:Response,next:NextFunction){
+export  function authMiddleware(req:Request,res:Response,next:NextFunction){
     const token = req.headers['authorization']
     console.log("this is token ",token)
     if(!token){
-        return res.status(401).json({
+        res.status(401).json({
             error: "Unauthorized"
         })
+        return
     }
     const decoded = jwt.verify(token,JWT_PUBLIC_KEY)
     console.log(decoded)
     if(!decoded|| !decoded.sub){
-        return res.status(403).json({
+        res.status(403).json({
             error:"maybe token is invalid"
         })
+        return 
     }
 
     req.userId= decoded.sub as string
